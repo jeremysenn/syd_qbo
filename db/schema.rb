f9066137974dd0f693e2fed49c490cfdf71a88fc
Jeremy Senn <senn.jeremy@gmail.com>
2015-08-27 07:21:24 -0700

    new user default to active and blank location

--- db/schema.rb
+++ db/schema.rb
@@ -97,20 +97,20 @@ ActiveRecord::Schema.define(version: 20150708133913) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",         null: false
-    t.string   "encrypted_password",     default: "",         null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,          null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "active",                 default: false
-    t.string   "location",               default: "Location"
+    t.boolean  "active",                 default: true
+    t.string   "location",               default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
