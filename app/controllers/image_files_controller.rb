4c96a154096f7dd0d4e3efc404479e73b788b41a
Jeremy Senn <senn.jeremy@gmail.com>
2015-09-02 12:58:12 -0700

    signature ajax form on purchase order page

--- app/controllers/image_files_controller.rb
+++ app/controllers/image_files_controller.rb
@@ -29,16 +29,15 @@ class ImageFilesController < ApplicationController
   # POST /image_files.json
   def create
     
-    @signature = params[:output]
-    
-    unless @signature.blank?
-      instructions = JSON.parse(@signature).map { |h| "line #{h['mx'].to_i},#{h['my'].to_i} #{h['lx'].to_i},#{h['ly'].to_i}" } * ' '
-      tempfile = Tempfile.new(["ticket_#{@image_file.ticket_number}_signature", '.png'])
-      Open3.popen3("convert -size 498x85 xc:transparent -stroke blue -draw @- #{tempfile.path}") do |input, output, error|
-          input.puts instructions
-      end
-      @image_file.file = tempfile
-    end
+#    @signature = params[:output]
+#    unless @signature.blank?
+#      instructions = JSON.parse(@signature).map { |h| "line #{h['mx'].to_i},#{h['my'].to_i} #{h['lx'].to_i},#{h['ly'].to_i}" } * ' '
+#      tempfile = Tempfile.new(["ticket_#{@image_file.ticket_number}_signature", '.png'])
+#      Open3.popen3("convert -size 498x85 xc:transparent -stroke blue -draw @- #{tempfile.path}") do |input, output, error|
+#          input.puts instructions
+#      end
+#      @image_file.file = tempfile
+#    end
 
     respond_to do |format|
       format.html { 
@@ -58,7 +57,19 @@ class ImageFilesController < ApplicationController
         end
         }
       format.js {
-        @image_file = ImageFile.create(image_file_params)
+        @signature = params[:output]
+        unless @signature.blank?
+          @image_file = ImageFile.new(image_file_params)
+          instructions = JSON.parse(@signature).map { |h| "line #{h['mx'].to_i},#{h['my'].to_i} #{h['lx'].to_i},#{h['ly'].to_i}" } * ' '
+          tempfile = Tempfile.new(["ticket_#{@image_file.ticket_number}_signature", '.png'])
+          Open3.popen3("convert -size 498x85 xc:transparent -stroke blue -draw @- #{tempfile.path}") do |input, output, error|
+              input.puts instructions
+          end
+          @image_file.file = tempfile
+          @image_file.save
+        else
+          @image_file = ImageFile.create(image_file_params)
+        end
       }
     end
   end
