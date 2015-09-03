8c8372d59aedf9c74d4803d86b82ce3b43f2a583
Jeremy Senn <senn.jeremy@gmail.com>
2015-09-03 15:40:35 -0600

    rails.cache for purchase_order edit page vendors and items

--- app/controllers/quickbooks_controller.rb
+++ app/controllers/quickbooks_controller.rb
@@ -1,5 +1,5 @@
 class QuickbooksController < ApplicationController
-
+  
   def authenticate
     callback = oauth_callback_quickbooks_url
     token = $qb_oauth_consumer.get_request_token(:oauth_callback => callback)
@@ -12,6 +12,26 @@ class QuickbooksController < ApplicationController
     session[:token] = quickbooks_authentication.token
     session[:secret] = quickbooks_authentication.secret
     session[:realm_id] = params['realmId']
+    
+    
+    ### Set Rails.cache for Quickbooks Vendors and Items ###
+    oauth_client = OAuth::AccessToken.new($qb_oauth_consumer, session[:token], session[:secret])
+    @vendor_service = Quickbooks::Service::Vendor.new
+    @vendor_service.access_token = oauth_client
+    @vendor_service.company_id = session[:realm_id]
+    
+    vendors = @vendor_service.query(nil, :per_page => 1000)
+    Rails.cache.delete("all_vendors")
+    Rails.cache.fetch("all_vendors") {Hash[vendors.map{ |v| [v.display_name,v.id] }]}
+    
+    @item_service = Quickbooks::Service::Item.new
+    @item_service.access_token = oauth_client
+    @item_service.company_id = session[:realm_id]
+    
+    items = @item_service.query(nil, :per_page => 1000)
+    Rails.cache.delete("all_items")
+    Rails.cache.fetch("all_items") {Hash[items.map{ |i| ["#{i.name} (#{i.description})",i.id] }]}
+    
     redirect_to root_url, notice: "Your QuickBooks account has been successfully linked."
   end
   
